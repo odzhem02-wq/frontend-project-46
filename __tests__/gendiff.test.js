965267abf4aba62dbf5a16393d3b807a48506dd0
dzhemdzhem <o.dzhem02@mail.ru>
2026-01-17 02:44:41 +0300

    step 5: add jest tests

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,24 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+test('gendiff for flat json', () => {
+  const filepath1 = path.join(__dirname, '..', '__fixtures__', 'file1.json');
+  const filepath2 = path.join(__dirname, '..', '__fixtures__', 'file2.json');
+
+  const expected = [
+    '{',
+    '  - follow: false',
+    '    host: hexlet.io',
+    '  - proxy: 123.234.53.22',
+    '  - timeout: 50',
+    '  + timeout: 20',
+    '  + verbose: true',
+    '}',
+  ].join('\n');
+
+  expect(genDiff(filepath1, filepath2)).toBe(expected);
+});
