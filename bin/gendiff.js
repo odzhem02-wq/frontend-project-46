40d62c1b3da0fa5219422b9250983b2e793534c2
dzhemdzhem <o.dzhem02@mail.ru>
2026-01-17 01:52:18 +0300

    step 2: add arguments and format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,10 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2, options) => {
+  });
 
 program.parse(process.argv);
