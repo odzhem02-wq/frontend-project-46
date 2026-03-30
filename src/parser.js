2d6624dd120a2c0d1fe7a625686dc9738f030710
dzhemdzhem <o.dzhem02@mail.ru>
2026-03-30 13:34:05 +0300

    final eslint fixes

--- src/parser.js
+++ src/parser.js
@@ -1,13 +1,13 @@
-import fs from 'fs'
-import path from 'path'
-import parse from './parsers/index.js'
+import fs from 'fs';
+import path from 'path';
+import parse from './parsers/index.js';
 
-const getAbsolutePath = filepath => path.resolve(process.cwd(), filepath)
+const getAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
 
-export default filepath => {
-  const absolutePath = getAbsolutePath(filepath)
-  const data = fs.readFileSync(absolutePath, 'utf-8')
-  const extname = path.extname(absolutePath)
+export default (filepath) => {
+  const absolutePath = getAbsolutePath(filepath);
+  const data = fs.readFileSync(absolutePath, 'utf-8');
+  const extname = path.extname(absolutePath);
 
-  return parse(data, extname)
-}
+  return parse(data, extname);
+};
