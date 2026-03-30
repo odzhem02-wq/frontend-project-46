7f6c240f6c2766e213f28832b0d6e22f5dfb052c
dzhemdzhem <o.dzhem02@mail.ru>
2026-03-30 13:28:46 +0300

    final fix lint formatting

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import parseFile from './parser.js';
-import buildDiff from './buildDiff.js';
-import getFormatter from './formatters/index.js';
+import parseFile from './parser.js'
+import buildDiff from './buildDiff.js'
+import getFormatter from './formatters/index.js'
 
 export default (filepath1, filepath2, formatName = 'stylish') => {
-  const data1 = parseFile(filepath1);
-  const data2 = parseFile(filepath2);
+  const data1 = parseFile(filepath1)
+  const data2 = parseFile(filepath2)
 
-  const diffTree = buildDiff(data1, data2);
-  const formatter = getFormatter(formatName);
+  const diffTree = buildDiff(data1, data2)
+  const formatter = getFormatter(formatName)
 
-  return formatter(diffTree);
-};
+  return formatter(diffTree)
+}
--- src/parsers/index.js
+++ src/parsers/index.js
@@ -1,14 +1,14 @@
-import parseJson from './json.js';
-import parseYaml from './yaml.js';
+import parseJson from './json.js'
+import parseYaml from './yaml.js'
 
 export default (data, extname) => {
   switch (extname) {
     case '.json':
-      return parseJson(data);
+      return parseJson(data)
     case '.yml':
     case '.yaml':
-      return parseYaml(data);
+      return parseYaml(data)
     default:
-      throw new Error(`Unknown format: ${extname}`);
+      throw new Error(`Unknown format: ${extname}`)
   }
-};
+}
